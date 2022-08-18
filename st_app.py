4449bf036085d6336f005df1afa7ca5c7092b6f3
sinfulnina <110668086+sinfulnina@users.noreply.github.com>
2022-08-18 08:00:52 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -3,6 +3,7 @@ import openai
 import nltk
 import os
 import emoji
+
 nltk.download('punkt')
 
 # EMOJI LINK https://carpedm20.github.io/emoji/
@@ -11,69 +12,85 @@ nltk.download('punkt')
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 with st.sidebar:
-
     st.image(image='https://www.sinful.dk/skin/frontend/sinful/sinful2020/images/logo_black.svg', width=100)
 
     temperature = st.slider(
         label='Temperature',
-        min_value=0.0, max_value=1.0, value=0.6, step=0.05, help="Controls Randomness: Lowering results in less random completions. As the temperature approaches zero, the model will become detministic and repetitive.")
+        min_value=0.0, max_value=1.0, value=0.6, step=0.05,
+        help="Controls Randomness: Lowering results in less random completions. As the temperature approaches zero, the model will become detministic and repetitive.")
 
     frequency_penalty = st.slider(
         label='Frequency Penalty',
-        min_value=-2.0, max_value=2.0, value=0.0, step=0.1, help="How much to penalize new tokens based on their existing frequency in the test so far. Decreases the model's likelihood to repeat the same line verbatim.")
+        min_value=-2.0, max_value=2.0, value=0.0, step=0.1,
+        help="How much to penalize new tokens based on their existing frequency in the test so far. Decreases the model's likelihood to repeat the same line verbatim.")
 
     max_tokens = st.slider(
         label='Maximum Token',
-        min_value=0, max_value=2000, value=50, step=50, help="The maximum number of tokens to generate. This does not tell the AI how long a text it should write, but how many tokens are generated, thus credited for.")
+        min_value=0, max_value=2000, value=50, step=50,
+        help="The maximum number of tokens to generate. This does not tell the AI how long a text it should write, but how many tokens are generated, thus credited for.")
 
+    # SESSION STATES DEFINED
+
+    # SESSION STATES TAB 1 - Sinful Generator
     if 'input_text' not in st.session_state:
         st.session_state['input_text'] = ''
-
-    if 'input_text2' not in st.session_state:
-        st.session_state['input_text2'] = ''
-
-    if 'input_text3' not in st.session_state:
-        st.session_state['input_text3'] = ''
-
-    if 'input_text4' not in st.session_state:
-        st.session_state['input_text4'] = ''
 
     if 'output_text' not in st.session_state:
         st.session_state['output_text'] = ''
 
+    # SESSION STATES TAB 2 - OpenAI Generator
+    if 'input_text2' not in st.session_state:
+        st.session_state['input_text2'] = ''
+
     if 'output_text2' not in st.session_state:
         st.session_state['output_text2'] = ''
+
+    # SESSION STATES TAB 3 - Product Description Generator
+    if 'input_text_product_name' not in st.session_state:
+        st.session_state['input_text_product_name'] = ''
+
+    if 'input_text_descriptors' not in st.session_state:
+        st.session_state['input_text_descriptors'] = ''
 
     if 'output_text3' not in st.session_state:
         st.session_state['output_text3'] = ''
 
+    # SESSION STATES TAB 4 - SEO Text Generator
+    if 'input_text_product_page' not in st.session_state:
+        st.session_state['input_text_product_page'] = ''
+
+    if 'input_text_section1' not in st.session_state:
+        st.session_state['input_text_section1'] = ''
+
+    if 'input_text_section2' not in st.session_state:
+        st.session_state['input_text_section2'] = ''
+
+    if 'input_text_section3' not in st.session_state:
+        st.session_state['input_text_section3'] = ''
+
     if 'output_text4' not in st.session_state:
         st.session_state['output_text4'] = ''
 
-    st.metric(label="Prompt Token Length", value=len(nltk.word_tokenize(st.session_state.input_text)))
-
-    st.metric(label="Output Token Length", value=len(nltk.word_tokenize(st.session_state.output_text)))
-
-    st.metric(label="Max Output Token Length", value=2018-len(nltk.word_tokenize(st.session_state.input_text)))
-
-tabs_titles = ["Sinful Generator","OpenAI Generator","Product Description Generator","SEO Text Generator"]
+# TAB BAR
+tabs_titles = ["Sinful Generator", "OpenAI Generator", "Product Description Generator", "SEO Text Generator"]
 
 tabs = st.tabs(tabs_titles)
 
+# TAB 1 - Sinful Generator
 with tabs[0]:
-
     st.header("Sinful Text Generator")
-    prompt = st.text_area("What do you want to write about? " + emoji.emojize(":writing_hand:"), key='input_text', help="This text generator is using the Sinful finetuned version of the Davinci engine")
+    prompt = st.text_area("What do you want to write about? " + emoji.emojize(":writing_hand:"), key='input_text',
+                          help="This text generator is using the Sinful finetuned version of the Davinci engine")
 
     if st.button('Submit', key='submit'):
         if prompt:
             response = openai.Completion.create(model="davinci:ft-sinful-2022-08-05-08-56-21",
-                                                #engine="text-davinci-001",
+                                                # engine="text-davinci-001",
                                                 temperature=temperature,
                                                 frequency_penalty=frequency_penalty,
                                                 prompt=prompt,
                                                 max_tokens=max_tokens,
-                                                #stop=["#"]
+                                                # stop=["#"]
                                                 )
             st.session_state['output_text'] = response['choices'][0]['text']
     st.write(st.session_state['output_text'])
@@ -85,20 +102,27 @@ with tabs[0]:
         key="sinful_text_download"
     )
 
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label="Prompt Token Length", value=len(nltk.word_tokenize(st.session_state.input_text)))
+    col2.metric(label="Output Token Length", value=len(nltk.word_tokenize(st.session_state.output_text)))
+    col3.metric(label="Max Output Token Length", value=2018 - len(nltk.word_tokenize(st.session_state.input_text)))
+
+# TAB 2 - OpenAI Generator
 with tabs[1]:
     st.header("OpenAI Text Generator")
-    prompt = st.text_area("What do you want to write about? " + emoji.emojize(":writing_hand:"), key='input_text2', help="This text generator is using the non-finetuned version of the Davinci engine")
+    prompt = st.text_area("What do you want to write about? " + emoji.emojize(":writing_hand:"), key='input_text2',
+                          help="This text generator is using the non-finetuned version of the Davinci engine")
 
     if st.button('Submit', key='submit2'):
         if prompt:
-            response = openai.Completion.create(#model="davinci:ft-sinful-2022-08-05-08-56-21",
-                                                engine="text-davinci-001",
-                                                temperature=temperature,
-                                                frequency_penalty=frequency_penalty,
-                                                prompt=prompt,
-                                                max_tokens=max_tokens,
-                                                #stop=["#"]
-                                                )
+            response = openai.Completion.create(  # model="davinci:ft-sinful-2022-08-05-08-56-21",
+                engine="text-davinci-001",
+                temperature=temperature,
+                frequency_penalty=frequency_penalty,
+                prompt=prompt,
+                max_tokens=max_tokens,
+                # stop=["#"]
+            )
             st.session_state['output_text2'] = response['choices'][0]['text']
     st.write(st.session_state['output_text2'])
 
@@ -109,25 +133,37 @@ with tabs[1]:
         key="openai_text_download"
     )
 
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label="Prompt Token Length", value=len(nltk.word_tokenize(st.session_state.input_text2)))
+    col2.metric(label="Output Token Length", value=len(nltk.word_tokenize(st.session_state.output_text2)))
+    col3.metric(label="Max Output Token Length", value=2018 - len(nltk.word_tokenize(st.session_state.input_text2)))
+
+# TAB 3 - Product Description Generator
 with tabs[2]:
     st.header("Product Description Generator")
 
-    product_name = st.text_input("Product Name", key='input_text_product_name', placeholder="E.g. Satisfyer Pro 2 Next Generation Clitoral Stimulator")
-    descriptions = st.text_input("Product Descriptors " + emoji.emojize(":star-struck:"), key='input_text_descriptors', placeholder="E.g Bestseller, sleek design, rechargeable, designer, new technologies..")
-    tone_of_voice = st.selectbox("Tone of Voice " + emoji.emojize(":speaking_head:"), ('Excited', 'Professional', 'Encouraging', 'Funny', 'Witty', 'Engaging', 'Creative'), key='input_tone_of_voice')
-    desired_length = st.slider("Desired Length",0, 500, 200, 25, help="Please remember to set the 'Maximum Token' length to be equal to Desired Length. It is not possible to tell the AI exactly how much text it should generate, though we can prompt a general length using this field.")
+    product_name = st.text_input("Product Name", key='input_text_product_name',
+                                 placeholder="E.g. Satisfyer Pro 2 Next Generation Clitoral Stimulator")
+    descriptions = st.text_input("Product Descriptors " + emoji.emojize(":star-struck:"), key='input_text_descriptors',
+                                 placeholder="E.g Bestseller, sleek design, rechargeable, designer, new technologies..")
+    tone_of_voice = st.selectbox("Tone of Voice " + emoji.emojize(":speaking_head:"),
+                                 ('Excited', 'Professional', 'Encouraging', 'Funny', 'Witty', 'Engaging', 'Creative'),
+                                 key='input_tone_of_voice')
+    desired_length = st.slider("Desired Length", 0, 500, 200, 25, key="input_desired_length",
+                               help="Please remember to set the 'Maximum Token' length to be equal to Desired Length. It is not possible to tell the AI exactly how much text it should generate, though we can prompt a general length using this field.")
 
-    prompt = ("Write a creative product description for a product page using the following Name, Descriptors, Tone of Voice and Desired Length." +
-        "\n\nNAME: " + product_name +
-        "\n\nDESCRIPTORS: " + descriptions +
-        "\n\nTONE OF VOICE: " + tone_of_voice +
-        "\n\nDESIRED LENGTH: " + str(desired_length) + " words" +
-        "\n\nPRODUCT DESCRIPTION:")
+    prompt = (
+            "Write a creative product description for a product page using the following Name, Descriptors, Tone of Voice and Desired Length." +
+            "\n\nNAME: " + product_name +
+            "\n\nDESCRIPTORS: " + descriptions +
+            "\n\nTONE OF VOICE: " + tone_of_voice +
+            "\n\nDESIRED LENGTH: " + str(desired_length) + " words" +
+            "\n\nPRODUCT DESCRIPTION:")
 
     if st.button('Submit', key='submit3'):
         if prompt:
             response = openai.Completion.create(model="davinci:ft-sinful-2022-08-05-08-56-21",
-                                                #engine="text-davinci-001",
+                                                # engine="text-davinci-001",
                                                 temperature=temperature,
                                                 frequency_penalty=frequency_penalty,
                                                 prompt=prompt,
@@ -142,36 +178,47 @@ with tabs[2]:
          "\n\nProduct Name: " + product_name +
          "\n\nProduct Descriptors: " + descriptions +
          "\n\nTone of Voice: " + tone_of_voice +
-         "\n\nDesired Length: " + str(desired_length) + " words"
-         "\n\nOUTPUT:\n\nProduct Description" + st.session_state['output_text3']),
+         "\n\nDesired Length: " + str(desired_length) + " words" +
+         "\n\nOUTPUT:\n\nProduct Description" + st.session_state[
+             'output_text3']),
         "product_description_generator.txt",
         key="product_description_download"
     )
 
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label="Prompt Token Length", value=len(nltk.word_tokenize(st.session_state.input_text_product_name)) + len(nltk.word_tokenize(st.session_state.input_text_descriptors)) + len(nltk.word_tokenize("Write a creative product description for a product page using the following Name, Descriptors, Tone of Voice and Desired Length. NAME: DESCRIPTORS: TONE OF VOICE: Excited DESIRED LENGTH: 200 words PRODUCT DESCRIPTION:")))
+    col2.metric(label="Output Token Length", value=len(nltk.word_tokenize(st.session_state.output_text3)))
+    col3.metric(label="Max Output Token Length", value=2018 - (len(nltk.word_tokenize(st.session_state.input_text_product_name)) + len(nltk.word_tokenize(st.session_state.input_text_descriptors)) + len(nltk.word_tokenize("Write a creative product description for a product page using the following Name, Descriptors, Tone of Voice and Desired Length. NAME: DESCRIPTORS: TONE OF VOICE: Excited DESIRED LENGTH: 200 words PRODUCT DESCRIPTION:"))))
+
+# TAB 4 - SEO Text Generator
 with tabs[3]:
     st.header("SEO Text Generator")
 
     product_page = st.text_input("Product Page Name", key='input_text_product_page', placeholder="E.g Vibrator")
-    section1 = st.text_input("Heading - Section 1", key='input_text_section1', placeholder="E.g What is a vibrator?").upper()
-    section2 = st.text_input("Heading - Section 2", key='input_text_section2', placeholder="E.g Why every women should own a vibrator").upper()
-    section3 = st.text_input("Heading - Section 3", key='input_text_section3', placeholder="E.g How to choose the perfect vibrator for you").upper()
+    section1 = st.text_input("Heading - Section 1", key='input_text_section1',
+                             placeholder="E.g What is a vibrator?").upper()
+    section2 = st.text_input("Heading - Section 2", key='input_text_section2',
+                             placeholder="E.g Why every women should own a vibrator").upper()
+    section3 = st.text_input("Heading - Section 3", key='input_text_section3',
+                             placeholder="E.g How to choose the perfect vibrator for you").upper()
 
-    prompt = ("Write a Search Engine Optimised text for a " + product_page + " product page with three sections. Use the following three headings as titles for each of the three sections: "
-               + "\n\n1. " + section1
-               + "\n\n2. " + section2
-               + "\n\n3. " + section3
-               + "\n\nEND")
+    prompt = (
+            "Write a Search Engine Optimised text for a " + product_page + " product page with three sections. Use the following three headings as titles for each of the three sections: "
+            + "\n\n1. " + section1
+            + "\n\n2. " + section2
+            + "\n\n3. " + section3
+            + "\n\nEND")
 
     if st.button('Submit', key='submit4'):
         if prompt:
-            response = openai.Completion.create(#model="davinci:ft-sinful-2022-08-05-08-56-21",
-                                                engine="text-davinci-001",
-                                                temperature=temperature,
-                                                frequency_penalty=frequency_penalty,
-                                                prompt=prompt,
-                                                max_tokens=max_tokens,
-                                                stop=["END"]
-                                                )
+            response = openai.Completion.create(  # model="davinci:ft-sinful-2022-08-05-08-56-21",
+                engine="text-davinci-001",
+                temperature=temperature,
+                frequency_penalty=frequency_penalty,
+                prompt=prompt,
+                max_tokens=max_tokens,
+                stop=["END"]
+            )
             st.session_state['output_text4'] = response['choices'][0]['text']
     st.write(st.session_state['output_text4'])
 
@@ -187,6 +234,11 @@ with tabs[3]:
         key="seo_text_download"
     )
 
-#with tabs[4]:
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label="Prompt Token Length", value=len(nltk.word_tokenize(st.session_state.input_text_product_page)) + len(nltk.word_tokenize(st.session_state.input_text_section1)) + len(nltk.word_tokenize(st.session_state.input_text_section3)) + len(nltk.word_tokenize(st.session_state.input_text_section3)) + len(nltk.word_tokenize("Write a Search Engine Optimised text for a product page with three sections. Use the following three headings as titles for each of the three sections: 1. 2. 3. END")))
+    col2.metric(label="Output Token Length", value=len(nltk.word_tokenize(st.session_state.output_text4)))
+    col3.metric(label="Max Output Token Length", value=2018 - (len(nltk.word_tokenize(st.session_state.input_text_product_page)) + len(nltk.word_tokenize(st.session_state.input_text_section1)) + len(nltk.word_tokenize(st.session_state.input_text_section3)) + len(nltk.word_tokenize(st.session_state.input_text_section3)) + len(nltk.word_tokenize("Write a Search Engine Optimised text for a product page with three sections. Use the following three headings as titles for each of the three sections: 1. 2. 3. END"))))
 
-    #st.header("Examples")
+# with tabs[4]:
+
+# st.header("Examples")
